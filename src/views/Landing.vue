5db1517427c229c5ef26a074fab72ba19fee2d18
guillaume-haerinck <guillaume.haerinck@outlook.com>
2022-10-15 19:49:35 +0200

    Replace about me by open source contribution in landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -11,53 +11,30 @@
       </div>
     </div>
 
-    <div class="fullheight" id="presentation">
+    <div class="fullheight last-projects">
       <div class="oblique-title-container">
         <div class="oblique-black"></div>
-        <h1 class="title">ABOUT ME</h1>
+        <h1 class="title">PROFESSIONAL HIGHLIGHTS</h1>
         <div class="oblique-black"></div>
       </div>
 
-      <div class="presentation-element wrap-reverse">
-        <div class="backgrounded presentation-text green">
-          <p>Great ideas are constantly shared accross the game development community so I do my best to keep updated and use these in my projects</p>
-        </div>
-
-        <div class="presentation-icon">
-          <img src="img/analysis.png" alt="Magnifying glass icon">
-        </div>
-      </div>
-
-      <div class="presentation-element">
-        <div class="presentation-icon">
-          <img src="img/artist.png" alt="Paint icon">
-        </div>
-
-        <div class="backgrounded presentation-text blue">
-          <p>I have an artistic knowledge which is a big help when I work on the visuals and collaborate with artists</p>
-        </div>
-      </div>
-
-      <div class="presentation-element wrap-reverse">
-        <div class="backgrounded presentation-text red">
-          <p>Performance is crucial to me which is why I always try to get closer to the metal and see how things really works</p>
-        </div>
-
-        <div class="presentation-icon">
-          <img src="img/milometer.png" alt="Meter icon">
-        </div>
+      <div class="last-projects-container">
+        <Project v-for="project in professionalProjects" 
+          :key="'project-' + project.name"
+          :project="project"
+        />
       </div>
     </div>
 
-    <div class="fullheight-min" id="last-projects">
+    <div class="fullheight-min last-projects">
       <div class="oblique-title-container">
         <div class="oblique-black"></div>
-        <h1 class="title">LAST PROJECTS</h1>
+        <h1 class="title">OPEN SOURCE CONTRIBUTION</h1>
         <div class="oblique-black"></div>
       </div>
 
-      <div id="last-projects-container">
-        <Project v-for="project in projects" 
+      <div class="last-projects-container">
+        <Project v-for="project in contributionProjects" 
           :key="'project-' + project.name"
           :project="project"
         />
@@ -85,7 +62,8 @@ export default defineComponent({
   },
   data() {
     return {
-      projects: [Projects["evil-age"], Projects["sakari"]]
+      professionalProjects: [Projects["sparks-of-hope"], Projects["scanrig"]],
+      contributionProjects: [Projects["open-engine"]]
     }
   }
 });
@@ -161,42 +139,13 @@ export default defineComponent({
   display: grid;
 }
 
-.presentation-element {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-wrap: wrap;
-}
-
-.presentation-element p {
-  margin: 0;
-}
-
-.wrap-reverse {
-  flex-wrap: wrap-reverse;
-}
-
-.presentation-text {
-  max-width: 600px;
-}
-
-.presentation-icon {
-  max-width: 125px;
-}
-
-.presentation-icon img {
-  width: 100%;
-  margin: 10px;
-}
-
 /* Last projects */
 
-#last-projects {
+.last-projects {
   display: grid;
 }
 
-#last-projects-container {
+.last-projects-container {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
